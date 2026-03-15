637fea90fcfc99d49752e81538c3594a9992abd9
san-jay-anand <ant.sanjay2008@gmail.com>
2026-03-16 00:12:57 +0530

    fix hardcoded localhost url

--- src/socket.js
+++ src/socket.js
@@ -2,23 +2,12 @@ import { io } from "socket.io-client";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
-let socket;
-
-try {
-  socket = io(BACKEND_URL, {
-    transports: ["polling"],
-    autoConnect: false,
-    reconnection: false,
-  });
-} catch (err) {
-  console.log("Socket not available");
-  socket = {
-    on: () => {},
-    off: () => {},
-    emit: () => {},
-    connect: () => {},
-    disconnect: () => {},
-  };
-}
+const socket = io(BACKEND_URL, {
+  transports: ["polling", "websocket"],
+  autoConnect: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
 
 export default socket;
